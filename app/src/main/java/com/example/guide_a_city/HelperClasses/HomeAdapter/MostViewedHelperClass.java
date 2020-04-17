dec296acc12ac4f76ae58d6361e99129b56852fc
Rishi <rishiraj619march@gmail.com>
2020-04-17 23:09:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/guide_a_city/HelperClasses/HomeAdapter/MostViewedHelperClass.java
@@ -0,0 +1,24 @@
+package com.example.guide_a_city.HelperClasses.HomeAdapter;
+
+public class MostViewedHelperClass {
+
+
+    int image;
+    String title,description;
+
+
+    public MostViewedHelperClass(int image, String title) {
+        this.image = image;
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+}
