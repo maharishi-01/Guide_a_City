98155f3b9ea4756bff16b7d3f1763146fa972e9a
Rishi <rishiraj619march@gmail.com>
2020-09-16 21:45:57 +0530

    Added Forget password page and update page

---
+++ app/src/main/java/com/example/guide_a_city/Common/LoginSignup/SetNewPassword.java
@@ -0,0 +1,16 @@
+package com.example.guide_a_city.Common.LoginSignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.guide_a_city.R;
+
+public class SetNewPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_new_password);
+    }
+}
